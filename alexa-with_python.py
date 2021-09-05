e9c91862ab881b5bf62c5f9b7d3ba3ffdb62bf93
Deepak sharma <88511824+d-e-ep@users.noreply.github.com>
2021-09-05 20:52:45 +0530

    Update alexa-with_python.py

--- alexa-with_python.py
+++ alexa-with_python.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -9,6 +11,7 @@ engine.say('i am your alexa')
 engine.say('what can i do for you')
 engine.runAndWait()
 
+
 try:
     with sr.Microphone() as source:
         print('listening...')
@@ -16,6 +19,24 @@ try:
         command = listener.recognize_google(voice)
         command = command.lower()
         if 'alexa' in command:
+            command = command.replace('alexa', '')
             print(command)
 except:
     pass
+  return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'paly' in command:
+        song = command.replace('play','')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+        
+    else'time' in command:
+        time = datetime.datetime.now().strftime('%i:%M %p')
+        print(time)
+        talk('current time is'+ time)
+                                                
+            
+run_alexa()
